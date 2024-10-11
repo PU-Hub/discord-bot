4a1c73567f33f746b47090e3743b96ef3550c8e2
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-10-11 12:31:34 +0800

    fix: register /food

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,6 @@
 import type { Command } from '@/class/command';
 
+import food from '$/food';
 import ping from '$/ping';
 
-export default [ping] as Command[];
+export default [food, ping] as Command[];
