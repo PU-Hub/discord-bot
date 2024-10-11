aa5fdc9e64be4832552e9207e6b36512e29cd4fc
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-10-11 17:07:56 +0800

    feat: db

---
+++ src/database/index.ts
@@ -0,0 +1,9 @@
+import { drizzle } from 'drizzle-orm/bun-sqlite';
+import { Database } from 'bun:sqlite';
+
+import * as schema from './schema';
+
+const dbPath = process.env['DATABASE_URL'];
+const db = new Database(dbPath, { create: true, strict: true });
+
+export default drizzle(db, { schema });
